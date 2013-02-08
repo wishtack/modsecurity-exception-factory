ba5efe96670b5185361975e4aef8f28e25a1e630
Younes <yjaaidi@gmail.com>
2013-02-08 01:39:29 +0100

    WIP: Correlation seems to work great!!! But it's really dirty!!! Need to clean that up...

--- modsecurity_exception_factory/correlation/correlation.py
+++ modsecurity_exception_factory/correlation/correlation.py
@@ -7,23 +7,115 @@
 # $Id$
 #
 
+from contracts import contract
 from synthetic.decorators import synthesizeConstructor, synthesizeMember
 
 @synthesizeConstructor()
 class Correlation:
 
-    def __init__(self, variableName, variableValue):
+    @contract
+    def __init__(self, variableName, variableValue = None, variableValueSet = None, subCorrelationList = None):
         """
     :type variableName: str
-    :type variableValue: unicode
+    :type variableValue: unicode|None
+    :type variableValueSet: Iterable|None
+    :type subCorrelationList: list|None
 """
         self._variableName = variableName
-        self._variableValueSet = set([variableValue])
-        self._subCorrelationList = []
+        
+        if variableValueSet is None:
+            self._variableValueSet = {variableValue}
+        else:
+            self._variableValueSet = variableValueSet
+            
+        if subCorrelationList is None:
+            subCorrelationList = []
+        self._subCorrelationList = subCorrelationList
 
     def extendSubCorrelation(self, correlationIterable):
         for correlation in correlationIterable:
             self.addSubCorrelation(correlation)
 
     def addSubCorrelation(self, correlation):
-        self._subCorrelationList.append(correlation)
+        # First sub correlation...
+        if len(self._subCorrelationList) == 0:
+            self._subCorrelationList.append(correlation)
+            return
+
+        # @todo ...checking that sub correlation have the same domain.
+
+        # ...otherwise.
+        lastCorrelation = self._subCorrelationList[-1]
+        if not lastCorrelation._hasOnlyOneBranch() or not correlation._hasOnlyOneBranch():
+            self._subCorrelationList.append(correlation)
+            return
+        
+        # Listing similar and different variables.
+        lastVariableDict = lastCorrelation._variableDict()
+        currentVariableDict = correlation._variableDict()
+        
+        lastKeyList = list(lastVariableDict.keys())
+        currentKeyList = list(currentVariableDict.keys())
+        lastKeyList.sort()
+        currentKeyList.sort()
+        if lastKeyList != currentKeyList:
+            raise Exception()
+        
+        similarVariableNameList = []
+        differentVariableNameList = []
+        for key in lastVariableDict.keys():
+            if currentVariableDict[key] == lastVariableDict[key]:
+                similarVariableNameList.append(key)
+            else:
+                differentVariableNameList.append(key)
+        
+        # Merging different variable (if there's only one)
+        subCorrelationList = []
+        if len(differentVariableNameList) == 1:
+            key = differentVariableNameList[0]
+            valueSet = currentVariableDict[key].union(lastVariableDict[key])
+            subCorrelationList.append(Correlation(key,
+                                                  variableValueSet = valueSet))
+
+        elif len(differentVariableNameList) > 1:
+            lastCorrelationChildrenList = []
+            currentCorrelationChildrenList = []
+            for key in differentVariableNameList:
+                lastCorrelation = Correlation(key,
+                                              variableValueSet = lastVariableDict[key],
+                                              subCorrelationList = lastCorrelationChildrenList)
+                lastCorrelationChildrenList = [lastCorrelation]
+
+                currentCorrelation = Correlation(key,
+                                                 variableValueSet = currentVariableDict[key],
+                                                 subCorrelationList = currentCorrelationChildrenList)
+                currentCorrelationChildrenList = [currentCorrelation]
+            subCorrelationList =  lastCorrelationChildrenList + currentCorrelationChildrenList
+        
+        for key in similarVariableNameList:
+            subCorrelationList = [Correlation(key,
+                                              variableValueSet = currentVariableDict[key],
+                                              subCorrelationList = subCorrelationList)]
+        del self._subCorrelationList[-1]
+        self._subCorrelationList.extend(subCorrelationList)
+
+    def _hasOnlyOneBranch(self):
+        subCorrelationCount = len(self._subCorrelationList)
+        if subCorrelationCount == 0:
+            return True
+        elif subCorrelationCount > 1:
+            return False
+        else:
+            return self._subCorrelationList[0]._hasOnlyOneBranch()
+
+    def _variableDict(self):
+        subCorrelationCount = len(self._subCorrelationList)
+        if subCorrelationCount == 0:
+            return {self._variableName: self._variableValueSet}
+        elif subCorrelationCount > 1:
+            raise Exception()
+        else:
+            variableDict = self._subCorrelationList[0]._variableDict()
+            variableDict[self._variableName] = self._variableValueSet
+            return variableDict
+        
--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -41,55 +41,97 @@ After merging, the following correlation tree:
 
 ...becomes: 
 
+
  a = a1
         b = b1
-                c = c1
-                        d = d1, d2, d3, d4
-                d = d1, d2, d3
-                        c = c2, c3, c4
-        c = c1, c2
-                d = d1, d2, d3
+                d = d4, d2, d3, d1
+                        c = c1
+                d = d2, d3, d1
+                        c = c3, c2, c4
+        d = d2, d3, d1
+                c = c2, c1
                         b = b2, b3
+
 """
-        d1 = Correlation('d', u"d1")
-        d2 = Correlation('d', u"d2")
-        d3 = Correlation('d', u"d3")
-        d4 = Correlation('d', u"d4")
-        
+        # a1 -> b1 -> c1
         c1 = Correlation('c', u"c1")
-        c1.extendSubCorrelation([d1, d2, d3, d4])
+        c1.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3"),
+                                 Correlation('d', u"d4")])
 
+        # a1 -> b1 -> c2
         c2 = Correlation('c', u"c2")
-        c2.extendSubCorrelation([d1, d2, d3])
+        c2.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3")])
 
+        # a1 -> b1 -> c4
         c4 = Correlation('c', u"c4")
-        c4.extendSubCorrelation([d1, d2, d3])
+        c4.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3")])
 
+        # a1 -> b1 -> c3
         c3 = Correlation('c', u"c3")
-        c3.extendSubCorrelation([d1, d2, d3])
-        
+        c3.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3")])
+
+        # a1 -> b1   
         b1 = Correlation('b', u"b1")
         b1.extendSubCorrelation([c1, c2, c4, c3])
 
+        # a1 -> b2 -> c1
+        c1 = Correlation('c', u"c1")
+        c1.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3")])
+
+        # a1 -> b2 -> c2
+        c2 = Correlation('c', u"c2")
+        c2.extendSubCorrelation([Correlation('d', u"d1"),
+                                 Correlation('d', u"d2"),
+                                 Correlation('d', u"d3")])
+
+        # a1 -> b2
         b2 = Correlation('b', u"b2")
         b2.extendSubCorrelation([c1, c2])
-        
+
+        # a1 -> b3 -> d1
         d1 = Correlation('d', u"d1")
-        d1.extendSubCorrelation([c1, c2])
+        d1.extendSubCorrelation([Correlation('c', u"c1"),
+                                 Correlation('c', u"c2")])
 
+        # a1 -> b3 -> d2
         d2 = Correlation('d', u"d2")
-        d2.extendSubCorrelation([c1, c2])
+        d2.extendSubCorrelation([Correlation('c', u"c1"),
+                                 Correlation('c', u"c2")])
 
+        # a1 -> b3 -> d3
         d3 = Correlation('d', u"d3")
-        d3.extendSubCorrelation([c1, c2])
-
+        d3.extendSubCorrelation([Correlation('c', u"c1"),
+                                 Correlation('c', u"c2")])
+        
+        # a1 -> b3
         b3 = Correlation('b', u"b3")
         b3.extendSubCorrelation([d1, d2, d3])
 
+        # a1
         a1 = Correlation('a', u"a1")
         a1.extendSubCorrelation([b1, b2, b3])
 
-        print(self._correlationRepr(a1))
+        self.assertEqual("""\
+a = a1
+        b = b1
+                d = d1, d2, d3, d4
+                        c = c1
+                d = d1, d2, d3
+                        c = c2, c3, c4
+        d = d1, d2, d3
+                c = c1, c2
+                        b = b2, b3
+""", self._correlationRepr(a1))
 
     def testMergeVariableDuplicate(self):
         pass
@@ -98,7 +140,9 @@ After merging, the following correlation tree:
         pass
 
     def _correlationRepr(self, correlation, indent = u""):
-        reprString = u"%s%s = %s\n" % (indent, correlation._variableName, list(correlation._variableValueSet))
+        variableValueList = list(correlation._variableValueSet)
+        variableValueList.sort()
+        reprString = u"%s%s = %s\n" % (indent, correlation._variableName, u", ".join(variableValueList))
         for subCorrelation in correlation._subCorrelationList:
-            reprString += self._correlationRepr(subCorrelation, indent + u"    ")
+            reprString += self._correlationRepr(subCorrelation, indent + u"        ")
         return reprString
